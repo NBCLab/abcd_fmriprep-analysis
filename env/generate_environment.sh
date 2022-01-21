35b6037f3466171ae92a0b2a9d8965011dd79fc3
JulioAPeraza <jpera054@fiu.edu>
2022-01-21 16:56:50 -0500

    add gitignore

--- env/generate_environment.sh
+++ env/generate_environment.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 module add miniconda3-4.5.11-gcc-8.2.0-oqs2mbg
-conda create -p /home/data/abcd/code/abcd_fmriprep-analysis/env/abcd pip
+conda create -p /home/data/abcd/code/abcd_fmriprep-analysis/env/abcd pip python=3.8
